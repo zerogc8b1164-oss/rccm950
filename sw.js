a6c7882aa2e26b2049ff5244169685dd54a9315a
zerogc8b1164-oss <zerogc8b1164@gmail.com>
2026-09-13 14:55:55 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='rccm-950-crosschecked12-select10-20260912';
+const CACHE='rccm-950-crosschecked13-current-rules-20260913';
 const ASSETS=['./','./index.html','./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
